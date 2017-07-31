60275c6fa1beb33b70285b9c7cca81631fe71b35
chandanvyas <chandanvyas@ibigrock.com>
2017-08-01 02:31:44 +0530

    webpack-first-draft

---
+++ js/button1.js
@@ -0,0 +1,7 @@
+import {abc} from './button2.js'
+
+$(document).ready(function(){
+    $(".btn1").on("click", function(){
+        alert("Button 1 is clicked");
+    });
+});
---
+++ js/button2.js
@@ -0,0 +1,5 @@
+export var abc = $(document).ready(function(){
+    $(".btn2").on("click", function(){
+        alert("Button 2 is clicked");
+    });
+});
