b57bac629a2310de5615a978972891bb27055d0d
chandanvyas <chandanvyas@ibigrock.com>
2017-08-04 02:40:19 +0530

    webpack final working code

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,17 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './js/button1.js',
+  entry: ['./js/button1.js','./js/button2.js','./resource/style.css'],
     output: {
-        path: path.resolve(__dirname, 'test1'),
+        path: path.resolve(__dirname, 'test2'),
         filename: 'bundle.js',
-        publicPath: "/test1"
+        publicPath: "/test2"
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
+                exclude: /node_module/,
                 use: [
                     'style-loader',
                     'css-loader'                    
